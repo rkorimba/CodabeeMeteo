69898aa527507759dced6017535bac2e113fec90
rkorimba <korimbacus.riad@gmail.com>
2018-05-24 12:23:25 +0200

    Mise en place

---
+++ CodabeeMeteo/Controller/LocalisationExtension.swift
@@ -0,0 +1,9 @@
+//
+//  LocalisationExtension.swift
+//  CodabeeMeteo
+//
+//  Created by Riad Korimbacus on 24/05/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import Foundation
