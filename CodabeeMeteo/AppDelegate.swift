1557836724d839a552907a67f5e4c8b010e46346
rkorimba <korimbacus.riad@gmail.com>
2018-05-28 11:28:21 +0200

    Background update meteo

--- CodabeeMeteo/AppDelegate.swift
+++ CodabeeMeteo/AppDelegate.swift
@@ -30,7 +30,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+       
+        if let meteoController = self.window?.rootViewController as? MeteoController {
+            meteoController.locationManager?.startUpdatingLocation()
+            print("Ça fonctionne")
+        }
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
